ff83e9b3df4be625e3fd7ad987bc3e368f8b0b19
AndreaParrini <andrea.parrini26@gmail.com>
2024-02-07 17:49:39 +0100

    add in app.vue, section on main
    section of all card and of filters
    add style in css

--- src/App.vue
+++ src/App.vue
@@ -26,19 +26,52 @@ export default {
   <AppHeader></AppHeader>
   <main>
     <div class="container">
-      <div class="row">
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xxl" v-for="card in cards" :key="card.id + '_card'">
-          <div>{{ card.name }}</div>
-          <div>{{ card.archetype }}</div>
-          <img :src="'https://images.ygoprodeck.com/images/cards_small/' + card.id + '.jpg'" alt="">
+      <section class="section_filters">
+        <select name="" id="">
+          <option value="alien"> Alien</option>
+        </select>
+      </section>
+      <section class="section_cards">
+        <div class="result">Found {{ cards.length }} cards</div>
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xxl" v-for="card in cards" :key="card.id + '_card'">
+            <div class="card">
+              <img :src="'https://images.ygoprodeck.com/images/cards_small/' + card.id + '.jpg'" alt="">
+              <div>{{ card.name }}</div>
+              <div>{{ card.archetype }}</div>
+            </div>
+
+          </div>
         </div>
-      </div>
+      </section>
     </div>
+
   </main>
 </template>
 
 <style>
 main {
   background-color: var(--yu-gi-oh-primary);
+
+  & .section_filters {
+    padding: 2rem;
+  }
+
+  & .section_cards {
+    background-color: var(--yu-gi-oh-light);
+    padding: 1rem 2rem;
+
+    & .result {
+      background-color: var(--yu-gi-oh-dark);
+      color: var(--yu-gi-oh-light);
+      padding: 1rem 1.5rem;
+      margin-bottom: -1rem;
+
+    }
+
+    & .card {
+      background-color: var(--yu-gi-oh-primary);
+    }
+  }
 }
 </style>
